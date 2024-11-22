4d6aa84e512f1305f2d8ebb43c72b240db793853
Tiago Paiva <paiva.tiagotp@gmail.com>
2024-11-22 02:00:04 +0000

    criei a estrutura do projeto

---
+++ src/app/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package app;
+
+/**
+ *
+ * @author Tiago Paiva
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // Código inicial do programa
+        System.out.println("Bem-vindo ao Calculador de Rotas!");
+    }
+
+}
